b52c254d71cf75c3137ae66bb01144f4a2632b8e
Cesar Augusto <devcesaraugusto97@gmail.com>
2025-08-30 09:41:35 -0300

    criando tela de login e cadastro com bug
    bug na hora de efetuar o login (nao esta logando com a conta que eu acabei decriar)

--- ecommerce/usuarios/forms.py
+++ ecommerce/usuarios/forms.py
@@ -18,3 +18,49 @@ class CadastroForm(UserCreationForm):
             'password1',
             'password2',
         ]
+        widgets = {
+            'tipo': forms.RadioSelect(choices=Usuario.TIPO_PESSOA),
+            'first_name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Nome'
+            }),
+            'last_name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Sobrenome'
+            }),
+            'cpf': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'CPF'
+            }),
+            'telefone': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Telefone'
+            }),
+            'endereco': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Endereço'
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'E-mail pessoal'
+            }),
+            'data_nascimento': forms.DateInput(attrs={
+                'class': 'form-control',
+                'type': 'date',
+                'placeholder': 'Data de nascimento'
+            }),
+            'preferencia': forms.Select(attrs={
+                'class': 'form-control'
+            }),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(CadastroForm, self).__init__(*args, **kwargs)
+        self.fields['password1'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Senha'
+        })
+        self.fields['password2'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Confirme a senha'
+        })
--- ecommerce/usuarios/views.py
+++ ecommerce/usuarios/views.py
@@ -1,18 +1,39 @@
 from django.shortcuts import render, redirect
-from .forms import CadastroForm
 from django.contrib.auth import login
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import CadastroForm
+
 
 def cadastro_view(request):
+    """Cadastro de novo usuário"""
     if request.method == 'POST':
         form = CadastroForm(request.POST)
         if form.is_valid():
             usuario = form.save()
-            login(request, usuario)
-            return redirect('home_public')  # ou outra página
+            login(request, usuario)  # Faz login automático após cadastro
+            messages.success(request, "Cadastro realizado com sucesso! Bem-vindo à Ganyk Store 🚀")
+            return redirect('home_public')  # ajuste a rota conforme seu sistema
+        else:
+            messages.error(request, "Houve um erro no formulário. Verifique os campos e tente novamente.")
     else:
         form = CadastroForm()
+
     return render(request, 'usuarios/cadastro.html', {'form': form})
 
-def login_view(request):
-    return render(request, 'usuarios/login.html')
 
+def login_view(request):
+    """Login de usuário já cadastrado"""
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            messages.success(request, f"Bem-vindo de volta, {user.username}! 🎉")
+            return redirect('home_public')
+        else:
+            messages.error(request, "Usuário ou senha inválidos. Verifique e tente novamente.")
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'usuarios/login.html', {'form': form})
